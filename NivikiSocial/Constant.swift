a61a84d5f982807e9e6cde663622d7edb4e7e072
Khoa Nguyen <vankhoa0603@gmail.com>
2016-10-26 16:53:26 +0700

    Login with FB and email

---
+++ NivikiSocial/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  NivikiSocial
+//
+//  Created by Khoa on 10/26/16.
+//  Copyright © 2016 Khoa. All rights reserved.
+//
+
+import Foundation
+
+
+
+// STATUS
+let STATUS_USER_NONEXIST = 17011
--- NivikiSocial/SignInVC.swift
+++ NivikiSocial/SignInVC.swift
@@ -12,20 +12,16 @@ import FBSDKCoreKit
 import Firebase
 class SignInVC: UIViewController {
 
+    
+    @IBOutlet weak var emailTF : UITextField!
+    @IBOutlet weak var passwordTF : UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
+ 
     
     @IBAction func fbLogin(_ sender: Any) {
-        
-        
     let facebookLogin = FBSDKLoginManager()
         facebookLogin.logIn(withReadPermissions: ["email"], from: self) { error, result in
             if error != nil{
@@ -37,13 +33,67 @@ class SignInVC: UIViewController {
             }
             else {
 
-                let credential = FBSDKAccessToken.current().tokenString
-                print("\(credential)")
-            
+                let token = FBSDKAccessToken.current().tokenString
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: token!)
+                self.firebaseLogin(credential: credential)
             }
+        }
+    }
+ 
+    
+    
+    @IBAction func emailLogin(_ sender: Any) {
+        if let email = emailTF.text, email != "" , let pwd = passwordTF.text , pwd != ""{
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (FIRUser, error) in
+                if error != nil{
+                    print( (error as! NSError).code == STATUS_USER_NONEXIST)
+                    self.showAlert(title: "User not found", msg: "This user wasn't created")
+                
+                FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                    
+                    print("Create user successfully")
+                    
+                })
+                    
+                }
+                // Success
+                else {
+                    print("Login success")
+                   
+                }
+            })
             
         }
-
+        else {
+            self.showAlert(title: "Error", msg: "Please type in email or password")
+        }
+    }
+    
+    
+    // MARK: Function
+    func showAlert(title : String, msg:  String){
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    func firebaseLogin(credential : FIRAuthCredential)
+    {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil{
+                print("Firebase login error")
+            }else{
+                print("Login successfully")
+            }
+        })
     }
 }
+
+
+
+
+
+
 
